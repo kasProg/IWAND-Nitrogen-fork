eb3092485d5140022c404242d215bbe89d717001
Doaa Aboelyazeed <97982507+DoaaAboelyazeed@users.noreply.github.com>
2025-08-03 14:02:59 -0400

    Update config.py

--- Scripts/Watershed_Boundary_Extraction/configs/config.py
+++ Scripts/Watershed_Boundary_Extraction/configs/config.py
@@ -6,7 +6,7 @@ HUC_no_lst = ['01','02','03','04','05',
               '16','17','18']
 
 #Define the required fields
-required_fields = ["COMID", "Monitoring","lat","lon", "AreaSqKM","wshd area","TotDASqKM", "Source", "GII", "WT"]
+required_fields = ["COMID", "Monitoring", "lat","lon" , "AreaSqKM", "wshd area", "TotDASqKM", "Source", "GII", "WT"]
 
 # Define the groups dictionary
 categories_dict = {   1: 'gII_name',
@@ -17,11 +17,12 @@ categories_dict = {   1: 'gII_name',
                   }
 
 # Define paths
-SWTrends_path       = "path to shape files from Watershed boundaries for study sites of the U.S. Geological Survey Surface Water Trends project" #https://data.usgs.gov/datacatalog/data/USGS:57a9e239e4b05e859be05534
-Huc_csv_path        = r".\data\HUC_{}\csv"
-output_path         = r".\data\HUC_{}\output_shapefiles"
-UScomids_path       = r".\data\HUC_{}\gauges_UScomids"
-catchment_path      = r"path to NHDPlusCatchment for all HUCs from NHDPlus"
-input_gaugesII_path = r"path to shape files from GAGES-II: Geospatial Attributes of Gages for Evaluating Streamflow by USGS" #https://data.usgs.gov/datacatalog/data/USGS:1d623649-ccb9-4238-8add-3174bc322fdf
+SWTrends_path       = "path to shape files from Watershed boundaries for study sites of the U.S. Geological Survey Surface Water Trends project" # https://data.usgs.gov/datacatalog/data/USGS:57a9e239e4b05e859be05534
+Huc_csv_path        = r"./data/HUC_{}/csv"
+output_path         = r"./data\HUC_{}/output_shapefiles"
+UScomids_path       = r"./data\HUC_{}/gauges_UScomids"
+catchment_path      = r"path to NHDPlusCatchment shapefiles for all HUCs from NHDPlus"# https://www.epa.gov/waterdata/get-nhdplus-national-hydrography-dataset-plus-data#Download
+input_gaugesII_path = r"path to shape files from GAGES-II: Geospatial Attributes of Gages for Evaluating Streamflow by USGS" # https://data.usgs.gov/datacatalog/data/USGS:1d623649-ccb9-4238-8add-3174bc322fdf
+
 
 
